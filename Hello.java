e02d1b47b7e8d855c5311fec19e8f7dce3e030c6
pranay2492 <pranaysharma2492@gmail.com>
2017-09-19 22:56:30 +0530

    thirdchanges

--- Hello.java
+++ Hello.java
@@ -4,7 +4,7 @@ public static void main(String args[])
 {
 for (int i=1;i<=5;i++)
 {
-System.out.println("New Git See my changes now second");
+System.out.println("New Git See my changes now third");
 System.out.println("Hello world.."+i);
 }
 }
