28b144ba615c895e4903e51a830bfbfff7d1918f
pranay2492 <pranaysharma2492@gmail.com>
2017-09-19 22:21:06 +0530

    firstcode

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello
+{
+public static void main(String args[])
+{
+for (int i=1;i<=5;i++)
+{
+System.out.println("New Git See my changes now 1");
+System.out.println("Hello world.."+i);
+}
+}
+}
